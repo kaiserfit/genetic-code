4b3a9acb2c63b5cc76737ba5a74a4f41e23c8c7a
KaiserFit <108109987+kaiserfit@users.noreply.github.com>
2022-08-10 16:03:44 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-// import './index.css';
-// import App from './App';
+
 import './quiz.css';
 import reportWebVitals from './reportWebVitals';
 
