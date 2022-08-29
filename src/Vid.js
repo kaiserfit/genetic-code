46f1504c9da55a1045e1614b3378e38a222f85b6
KaiserFit <108109987+kaiserfit@users.noreply.github.com>
2022-08-29 15:47:44 +0800

    Update Vid.js

--- src/Vid.js
+++ src/Vid.js
@@ -31,7 +31,7 @@ const Vid = ({setRoute}) => {
     const [userPlay, setUserPlay] = useState(false);
     const [paused, setPaused] = useState(false);
     const [vslWatched, setWatch] = useState(false);
-    const [hook6, setHook6] = useState(true);
+    const [hook6, setHook6] = useState(false);
     const [hook1, setHook1] = useState(false);
     const [videoTime, setTime] = useState(0);
    
