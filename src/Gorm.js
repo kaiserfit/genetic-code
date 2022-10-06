8f2699cb8d6e4dc69abc5166d41bf64d8e993ac4
KaiserFit <108109987+kaiserfit@users.noreply.github.com>
2022-10-06 08:29:22 +0800

    Update Gorm.js

--- src/Gorm.js
+++ src/Gorm.js
@@ -25,7 +25,7 @@ export default function Gorm({priceId, price, customerDetails, setRoute, bot, p}
  
   useEffect(() => {
     // Create PaymentIntent as soon as the page loads
-    fetch("https://queenformula.net/apiv4/index.php/createPm/test", {
+    fetch("https://queenformula.net/apiv4/index.php/createPm", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ priceId: priceId, price: price })
@@ -55,36 +55,9 @@ export default function Gorm({priceId, price, customerDetails, setRoute, bot, p}
     if (customerPaymentMethod !== ""){
         var ob = (orderBump) ? "true" : "false";
 
-        // axios({
-        //   method: "POST",
-        //   url: "https://queenformula.net/sendy/api/subscribers/delete.php",
-        //   data: {api_key:'cLlL1tqV0HmMII2c1G6I',list_id:'ivjFaa7aAJo8921ooWPJcZ892w', email:customerDetails[0].email},
-        //   headers: { "Content-Type": "multipart/form-data" },
-        // })
-
-        // axios({
-        //   method: "POST",
-        //   url: "https://queenformula.net/sendy/api/subscribers/delete.php",
-        //   data: {api_key:'cLlL1tqV0HmMII2c1G6I',list_id:'nTKk4tvGKlBzm4zrkh9Agw', email:customerDetails[0].email},
-        //   headers: { "Content-Type": "multipart/form-data" },
-        // })
-
-        // var ipx = localStorage.getItem('_uip')
-        // axios({
-        //   method: "POST",
-        //   url: "https://queenformula.net/sendy/subscribe",
-        //   data: {
-        //     api_key: 'cLlL1tqV0HmMII2c1G6I',
-        //     name: customerDetails[0].name,
-        //     email: customerDetails[0].email,
-        //     list: 'gvPodrI1haUinwHFG2L2763g',
-        //     referrer: window.location.href,
-        //     hp: '',
-        //     ipaddress: ipx,
-        //   },
-        //   headers: { "Content-Type": "multipart/form-data" },
-        // })
-        fetch('https://queenformula.net/apiv4/index.php/createCustomer/test', {
+       
+      
+        fetch('https://queenformula.net/apiv4/index.php/createCustomer', {
           method: 'POST',
           headers: {
             'Accept': 'application/json',
